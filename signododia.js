8982a701a699351d262c2e4a644597b2cfa5e6ac
Jonathan <https://github.com/jonathaneduardopsv>
2023-04-24 11:11:06 -0300

    Parametros

--- signododia.js
+++ signododia.js
@@ -12,3 +12,38 @@ let colecao_signos = [
     {"Nome": "Sagitário", "DataInicio": "11-22","DataFim":"12-21"},
     {"Nome": "Capricórnio", "DataInicio": "12-22","DataFim":"01-19"}
 ];
+
+const verifica_data_range =
+ (data, data_inicio, data_fim, tipo_comparacao) => {
+
+    if ( tipo_comparacao == "and" ){
+        return (data >= data_inicio && data <= data_fim);
+    } else if( tipo_comparacao == "or" ){
+        return (data >= data_inicio || data <= data_fim);
+    }
+ }
+
+const retorna_signo = (signos) => {
+
+    let data = new Date();
+
+    let ano = data.getFullYear();
+
+    for (const signo of signos){
+
+    let data_inicio_signo = new Date(ano + "-" + signo["DataInicio"] + " 00:00:00");
+    let data_fim_signo = new Date(ano + "-" + signo["DataFim"] + " 23:59:59");
+
+    let tipo_comparacao = signo["DataInicio"] == "12-22" ? "or" : "and";
+
+        if (verifica_data_range(data, data_inicio_signo, data_fim_signo, tipo_comparacao)){
+            return signo["Nome"];
+        } 
+    }
+}
+
+let data_app = new Date();
+
+const nome_signo = retorna_signo(colecao_signos, data_app);
+
+console.log("O signo de hoje é: " + nome_signo);
